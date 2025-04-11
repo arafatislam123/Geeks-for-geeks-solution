8b56712011c0a325b9172cd6ec9d51ebe2d7fb0e
Arafat420 <arafatpstu7@gmail.com>
2025-04-12 00:04:51 +0600

    first commit

---
+++ Find the Frequency.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define nl '\n'
+#define pii pair<int, int>
+#define pll pair<long long int, long long int>
+#define ALL(v) (v).begin(), (v).end()
+#define rALL(v) (v).rbegin(), (v).rend()
+
+#define show(x) cout << #x << " : " << x << endl
+
+void solve(int tc)
+{
+              
+}
+
+int main()
+{
+ ios_base::sync_with_stdio(0);
+  cin.tie(0);
+    int tc = 1;
+    //cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+        solve(i);
+}
