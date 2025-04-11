c57bed91730a157e3fd04d8906dfb0bcefad930e
Arafat420 <arafatpstu7@gmail.com>
2025-04-12 01:41:14 +0600

    another way frequency counting

---
+++ Find the Frequency alternative.cpp.cpp
@@ -0,0 +1,58 @@
+//problem arrays element frequency counting ****
+//input***
+// 5
+// 5 7 2 7 2 5
+// output***
+// 2 2 
+// 5 1 
+// 7 2
+
+#include <bits/stdc++.h>
+    using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define nl '\n'
+#define pii pair<int, int>
+#define pll pair<long long int, long long int>
+#define ALL(v) (v).begin(), (v).end()
+#define rALL(v) (v).rbegin(), (v).rend()
+
+#define show(x) cout << #x << " : " << x << endl
+
+void solve(int tc)
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    map<int, int> freq;
+    for (int i = 0;i<n;i++)
+    {
+        cin >> v[i];
+        freq[v[i]]++;
+        }
+   
+
+    // for(auto x:freq)
+    // {
+    //     cout << x.first << " " << x.second << nl;
+    // }
+    // above method is right way but i am doing another way
+        for (auto it = freq.begin(); it != freq.end(); it++)
+        {
+            cout << it->first << " " << it->second << '\n';
+        }
+}
+
+int main()
+{
+ ios_base::sync_with_stdio(0);
+  cin.tie(0);
+    int tc = 1;
+    //cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+        solve(i);
+}
